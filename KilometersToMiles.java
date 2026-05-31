1f766eacf64135a6dc88e7ef2360692a24c95687
sappasivaji <sivajisappa00@gmail.com>
2026-05-31 11:57:02 +0530

    Add files via upload

---
+++ KilometersToMiles.java
@@ -0,0 +1,24 @@
+package com.shiva.demo;
+
+import java.util.Scanner;
+
+
+
+
+public class KilometersToMiles {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("Enter distance in kilometers: ");
+		double kilometers = sc.nextDouble();
+		
+		
+		//double kilometers = 10.0; // Example value in kilometers
+		double miles = kilometers * 0.621371; // Conversion factor from kilometers to miles
+		
+		System.out.println(kilometers + " kilometers is equal to " + miles + " miles.");
+
+	}
+
+}
