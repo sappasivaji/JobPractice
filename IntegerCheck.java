0be743ce90014fb9d0dbe92e4a120ae92a77e483
sappasivaji <sivajisappa00@gmail.com>
2026-06-03 21:37:02 +0530

    Add files via upload

---
+++ IntegerCheck.java
@@ -0,0 +1,22 @@
+package com.shiva.demo;
+import java.util.Scanner;
+
+
+public class IntegerCheck {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+      Scanner sc = new Scanner(System.in);
+      
+      System.out.print("Enter a number: ");
+      
+      if (sc.hasNextInt()) {
+		  int number = sc.nextInt();
+		  System.out.println("You entered an integer: " + number);
+	  } else {
+		  System.out.println("You did not enter an integer.");
+	  }
+       sc.close();
+	}
+
+}
